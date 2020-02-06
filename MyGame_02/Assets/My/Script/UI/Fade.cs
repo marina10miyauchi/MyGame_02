dc6609b39dfb6cdcb9ff501ea9b1f2349db78a50
Marina_Miyauchi <mari01miya@gmail.com>
2020-02-07 00:19:37 +0900

    フェード用スクリプトとステージセレクト選択スクリプト　シーン

---
+++ MyGame_02/Assets/My/Script/UI/Fade.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum FadeType
+{
+    NoFade,     //フェードしない
+    FadeIn,     //フェードイン
+    FadeOut,    //フェードアウト
+}
+
+public class Fade : MonoBehaviour
+{
+    [SerializeField,Header("フェードスピード")]
+    float m_fadeSpeed = 1f;
+
+    [SerializeField, Header("フェード状態")]
+    FadeType m_fade=FadeType.FadeOut;
+    [SerializeField,Header("フェード用Image")]
+    Image m_FadeImage;              //フェード用
+
+    float m_FadeAlpha;          //フェード用キャンバスのα値
+    float m_time = 0;           //フェード用時間
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        m_FadeImage = GetComponentInChildren<Image>();
+    }
+    private void Update()
+    {
+        FadeState();
+    }
+    void FadeState()
+    {
+        switch (m_fade)
+        {
+            case FadeType.NoFade:
+                break;
+            case FadeType.FadeIn:
+                FadeIn();
+                break;
+            case FadeType.FadeOut:
+                FadeOut();
+                break;
+        }
+    }
+    public void ChangeFade(FadeType changeState)
+    {
+        if (changeState == m_fade) return;
+        m_FadeImage.transform.parent.gameObject.SetActive(true);
+        m_fade = changeState;
+    }
+    //true→フェードイン時のチェック　false→フェードアウト時のチェック
+    bool CheckFadeEnd(bool fadeIn)
+    {
+        if (fadeIn && m_FadeImage.color.a >= 1)
+        {
+            return true;
+        }
+        else if (!fadeIn && m_FadeImage.color.a <= 0)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    void FadeIn()
+    {
+       // m_time += m_fadeSpeed * Time.deltaTime;
+        m_FadeAlpha = m_fadeSpeed * Time.deltaTime;
+
+        Color fadeColor = m_FadeImage.color;
+
+        fadeColor.a += m_FadeAlpha;
+        m_FadeImage.color = fadeColor;
+        //フェード終了か？
+        if (CheckFadeEnd(true))
+        {
+            ChangeFade(FadeType.NoFade);
+            m_FadeImage.transform.parent.gameObject.SetActive(false);
+            m_time = 0;
+        }
+            
+    }
+    void FadeOut()
+    {
+        m_FadeAlpha = m_fadeSpeed * Time.deltaTime;
+        Color fadeColor = m_FadeImage.color;
+
+        fadeColor.a -= m_FadeAlpha;
+        m_FadeImage.color = fadeColor;
+        //フェード終了か？
+        if (CheckFadeEnd(false))
+        {
+            ChangeFade(FadeType.NoFade);
+            m_FadeImage.transform.parent.gameObject.SetActive(false);
+            m_time = 0;
+        }
+            
+    }
+
+}
---
+++ MyGame_02/Assets/My/Script/UI/TurnUI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TurnUI : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField,Header("ステージの名前が書いてあるUI")]
+    GameObject m_stageNameUI;
+    [SerializeField, Header("ステージの写真が載っているUI")]
+    GameObject m_stageImageUI;
+
+    StageSelectUI m_nameSelect;
+    StageSelectUI m_imageSelect;
+
+    [SerializeField,Header("上向き移動用か")]
+    bool m_IsUP;
+    bool m_onClick;
+
+    float m_angle = 0;
+    float m_StageNam = 0;
+
+    float m_prevOffsetAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_nameSelect = m_stageNameUI.GetComponent<StageSelectUI>();
+        m_imageSelect = m_stageImageUI.GetComponent<StageSelectUI>();
+        m_prevOffsetAngle = m_nameSelect.OffsetAngle;
+        
+    }
+    private void Update()
+    {
+        if (!m_onClick) return;
+        if (m_IsUP)
+            NextStage(-1);
+        else
+            NextStage(1);
+    }
+    public void OnPointerClick(PointerEventData pointerData)//マウスでクリックされたときの処理
+    {
+        m_onClick = true;
+    }
+    void NextStage(float changeValue)
+    {
+        if (Mathf.Abs(m_angle) <=Mathf.Abs(m_prevOffsetAngle+ m_nameSelect.TurnAngle))
+        {
+            m_nameSelect.OffsetAngle += changeValue;
+            m_imageSelect.OffsetAngle += changeValue;
+            m_angle += changeValue;
+
+        }
+        else
+        {
+            m_prevOffsetAngle += m_nameSelect.TurnAngle;
+            m_onClick = false;
+        }
+
+    }
+
+}
