d932bc2663b1df07e8dad1bdf774cb130647a2c9
Marina_Miyauchi <mari01miya@gmail.com>
2020-02-25 19:21:17 +0900

    オプション画面

---
+++ MyGame_02/Assets/My/Script/UI/Option.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class Option : MonoBehaviour
+{
+    [SerializeField,Header("オプションを開いているか")]
+    bool m_isOpen=false;
+    [SerializeField, Header("プレイヤー人数テキスト")]
+    Text m_playerCountText;
+    [SerializeField, Header("BGM_text")]
+    Text m_BGM;
+    [SerializeField, Header("SE_text")]
+    Text m_SE;
+
+    RectTransform m_optionTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_optionTransform = GetComponent<RectTransform>();
+        m_playerCountText.text = GameData.Instance.PlayerCount.ToString();
+    }
+
+    // Update is called once per frame
+    public void Change()
+    {
+        if (m_isOpen)//開いている状態の場合
+            Close();
+        else//開いていない状態の場合
+            Open();
+    }
+    void Open()
+    {
+        m_optionTransform.DOAnchorPosX(0, 0.5f);
+        m_isOpen = true;
+        
+    }
+    void Close()
+    {
+        m_optionTransform.DOAnchorPosX(287, 0.5f);
+        m_isOpen = false;
+    }
+    //プレイヤー人数の変動
+    //add=true → 増加 add=false → 減少
+    public void Count(bool add)
+    {
+        if (!m_isOpen) return;
+        if (add)
+            GameData.Instance.AddPlayer();
+        else
+            GameData.Instance.DecrementPlayer();
+
+        m_playerCountText.text = GameData.Instance.PlayerCount.ToString();
+       
+    }
+    //public void BGMSetting(bool add)
+    //{
+    //    if (!m_isOpen) return;
+    //    if (add)
+    //        SoundManager.Instance.BGMVolume=()
+
+    //}
+}
